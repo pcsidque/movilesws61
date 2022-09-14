e1cbfee8ed1e06b3ea186a92f85280141910e4bf
pcsidque <pcsidque@upc.edu.pe>
2022-09-14 15:27:12 -0500

    Py Agenda con Rv - v2

---
+++ AppAgendaRv/app/src/main/java/com/example/appagendarv/ContactActivity.kt
@@ -0,0 +1,45 @@
+package com.example.appagendarv
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import com.google.android.material.textfield.TextInputEditText
+
+class ContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.contact_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        saveContact()
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun saveContact() {
+        val name = findViewById<TextInputEditText>(R.id.etName)
+        val telephone = findViewById<TextInputEditText>(R.id.etTelephone)
+
+        val namefin = name.text.toString()
+        val telephonefin = telephone.text.toString()
+
+        val intent = Intent()
+
+        intent.putExtra("keyName", namefin)
+        intent.putExtra("keyTelephone", telephonefin)
+
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
+}
--- AppAgendaRv/app/src/main/java/com/example/appagendarv/MainActivity.kt
+++ AppAgendaRv/app/src/main/java/com/example/appagendarv/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.appagendarv
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    var contact = ArrayList<Contact>()
+    var contacts = ArrayList<Contact>()
+    val REQUEST_CODE = 1
 
     //creamos el adapter
-    var contactAdapter = com.example.appagendarv.ContactAdapter(contact)
+    var contactAdapter = com.example.appagendarv.ContactAdapter(contacts)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +34,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadContacts() {
-        contact.add(Contact("Luisa", "123456"))
-        contact.add(Contact("Yovana", "663456"))
-        contact.add(Contact("Yadira", "883456"))
-        contact.add(Contact("Nayruth", "999456"))
+        contacts.add(Contact("Luisa", "123456"))
+        contacts.add(Contact("Yovana", "663456"))
+        contacts.add(Contact("Yadira", "883456"))
+        contacts.add(Contact("Nayruth", "999456"))
+    }
+
+    //"infla" el menu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+
+        return true
+    }
+
+    //dar la accion
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val intent = Intent(this, ContactActivity::class.java)
+        //startActivity(intent)
+        startActivityForResult(intent, REQUEST_CODE)
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK){
+                val name = data!!.getStringExtra("keyName")
+                val telephone = data!!.getStringExtra("keyTelephone")
+                val contact = Contact(name, telephone)
+
+                contacts.add(contact)
+            }
+        }
     }
 }
