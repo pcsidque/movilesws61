32b47d34909a9ab84249467fec540e213b168600
pcsidque <pcsidque@upc.edu.pe>
2022-09-28 13:52:32 -0500

    App contact con Roomv2

--- AppPersistenciaRoom/app/src/main/java/com/example/apppersistenciaroom/ContactActivity.kt
+++ AppPersistenciaRoom/app/src/main/java/com/example/apppersistenciaroom/ContactActivity.kt
@@ -2,10 +2,24 @@ package com.example.apppersistenciaroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
 
 class ContactActivity : AppCompatActivity() {
+
+    lateinit var contact: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+    }
+
+    fun saveContact(){
+        val valor_name = findViewById<TextInputEditText>(R.id.etName)
+        val valor_telephone = findViewById<TextInputEditText>(R.id.etTelephone)
+
+        val name = valor_name.text.toString()
+        val telephone = valor_telephone.text.toString()
+
+        contact = User(null, name, telephone)
     }
 }
--- AppPersistenciaRoom/app/src/main/java/com/example/apppersistenciaroom/Usuario.kt
+++ AppPersistenciaRoom/app/src/main/java/com/example/apppersistenciaroom/Usuario.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class User (
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val id: Int?,
 
     @ColumnInfo
     val name: String,
